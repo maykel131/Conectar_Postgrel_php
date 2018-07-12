64ea0fafa9513de61cb1d50be1eb708b2c5bd928
Maykel Developer <33340492+maykel131@users.noreply.github.com>
2018-07-12 15:12:50 -0300

    Update postgre_conec.php

--- postgre_conec.php
+++ postgre_conec.php
@@ -11,16 +11,6 @@ class database{
     var $last_id;
 
 
-    function database($db, $user, $pass="nopass",$host="localhost", $port=5432){
-        $this->db_HOST=$host;
-        $this->db_PORT=$port;
-        $this->db_USER=$user;
-        $this->db_PASS=$pass;
-        $this->db=$db;
-        
-        $this->connect_db();
-    }
-
     function connect_db(){
         $connect_string = "host=".$this->db_HOST." port=".$this->db_PORT." user=".$this->db_USER." password=".$this->db_PASS." dbname=".$this->db;
         $this->dbc = pg_connect($connect_string);
